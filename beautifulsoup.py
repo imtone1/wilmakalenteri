1678de4cdb4ed8019981ac74d5d4b9cb280667dd
imtone <88165529+imtone1@users.noreply.github.com>
2023-12-12 11:21:38 +0200

    google kalenterista haku

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -10,6 +10,18 @@ from datetime import datetime, timedelta, timezone
 
 from pymongo import MongoClient
 
+calendarID = os.environ["CALENDAR_FAMILY"]
+
+#Google API
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
 load_dotenv()
 
 def wilma_exams(login_req, session):
@@ -105,7 +117,7 @@ def wilma_exams(login_req, session):
         # print(json_data)
 
     else:
-        print("Oppilasta ei löytynyt")
+        print("There is no such student.")
 
 def wilma_signin():
     login_url = os.environ["WILMA_URL"]
@@ -146,12 +158,71 @@ def get_items_mongodb(collection, query={}):
     for doc in documents:
         print(doc)
 
-def main():
-    wilma_exams(*wilma_signin())
-    one_minute_ago = datetime.now() - timedelta(hours=10, minutes=1)
-    query = {"created": {"$gte": one_minute_ago}}
+#Kirjautuminen Googlen kalenteriin
+def google_calendar_token():
+    """Tokens Google API
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+      creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+      if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+      else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            "credentials.json", SCOPES
+        )
+        creds = flow.run_local_server(port=0)
+      # Save the credentials for the next run
+      with open("token.json", "w") as token:
+        token.write(creds.to_json())
+    # Call the Calendar API
+    service = build("calendar", "v3", credentials=creds)
 
-    get_items_mongodb(connect_mongodb("kokeet"), query)
+    return service
+
+def show_calendar_events(calendarID="primary"):
+    try:
+        service = google_calendar_token()
+       
+        now = datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+        print("Getting the upcoming 10 events")
+        events_result = (
+                service.events()
+                .list(
+                    calendarId=calendarID,
+                    timeMin=now,
+                    maxResults=10,
+                    singleEvents=True,
+                    orderBy="startTime",
+                )
+                .execute()
+            )
+        events = events_result.get("items", [])
+
+        if not events:
+            print("No upcoming events found.")
+            return
+
+            # Prints the start and name of the next 10 events
+        for event in events:
+            start = event["start"].get("dateTime", event["start"].get("date"))
+            print(start, event["summary"])
+
+    except HttpError as error:
+        print(f"An error occurred in Google Calendar showing event: {error}")
+
+def main():
+    # wilma_exams(*wilma_signin())
+    # one_minute_ago = datetime.now() - timedelta(hours=10, minutes=1)
+    # query = {"created": {"$gte": one_minute_ago}}
+
+    # get_items_mongodb(connect_mongodb("kokeet"), query)
+    show_calendar_events(calendarID)
 
 if __name__ == "__main__":
   main()
